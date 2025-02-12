abb8f35ffa119b7588f0cf5db49c5d65be992521
leobenTaningco <leotaningco@gmail.com>
2025-02-12 16:38:08 +0800

    copied the login layout from figma

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,21 @@
 import { TouchableOpacity, StyleSheet, TextInput } from 'react-native';
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View} from '@/components/Themed';
 import React from 'react';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>UNITRAFFIC</Text>
-      <TextInput style={styles.input} placeholder="Username"></TextInput>
-      <TextInput style={styles.input} placeholder="Password"></TextInput>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.textButton}>Login</Text>
-      </TouchableOpacity>
+      <View style={styles.containerBox}>
+        <Text style={styles.title}>UNITRAFFIC</Text>
+        <TextInput style={styles.input} placeholder="Username"></TextInput>
+        <TextInput style={styles.input} placeholder="Password"></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgotPassword}>forgot password?</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,6 +26,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerBox: {
+    borderWidth: 1.5,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 15,
+    paddingHorizontal: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -29,7 +44,7 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    width: 200,
+    width: 250,
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
@@ -39,7 +54,7 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    width: 200,
+    width: 250,
     height: 40,
     borderWidth: 1,
     backgroundColor: "black",
@@ -52,6 +67,13 @@ const styles = StyleSheet.create({
   textButton: {
     color: 'white',
     fontWeight: 'bold',
+  },
+
+  forgotPassword: {
+    fontSize: 14,
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+    color: "black",
   }
 
 });
